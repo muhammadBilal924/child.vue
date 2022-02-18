15bfcf1b9c2b54538eeb42750acb97be4f3c7d27
muhammadBilal924 <mbilal60924@gmail.com>
2022-02-18 18:47:56 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <h1>{{title}}</h1>
+    <Child_parent v-on:changeTitle="updateTitle($event)" />
+  </div>
+</template>
+<script>
+import Child_parent from './components/Child_parent.vue'
+export default {
+  name: 'App',
+  components: {
+    Child_parent
+  },
+ data() {
+   return {
+     title:'Bilal Khan'
+   }
+ },
+  methods:{
+    updateTitle(title)
+    {
+      this.title=title
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Child_parent.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+   <button v-on:click="updateTitle">Update Parent Title</button>
+    </div>
+</template>
+
+<script>
+    export default {
+      name:"Child_parent",
+      methods:{
+          updateTitle(){
+           this.$emit('changeTitle',"Parent Update")   
+          }
+      }  
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
